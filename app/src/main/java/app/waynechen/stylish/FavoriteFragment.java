e1ad2ac16233ae293cd58c9a94890f4d94b3d781
Michael Lee <michael830713@gmail.com>
2019-03-19 21:12:09 +0800

    create new adapter and fragment

---
+++ app/src/main/java/app/waynechen/stylish/FavoriteFragment.java
@@ -0,0 +1,148 @@
+package app.waynechen.stylish;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.waynechen.stylish.catalog.item.CatalogItemContract;
+import app.waynechen.stylish.component.GridSpacingItemDecoration;
+import app.waynechen.stylish.data.source.bean.GetProductList;
+import app.waynechen.stylish.util.Constants;
+
+import static app.waynechen.stylish.MainMvpController.ACCESSORIES;
+import static app.waynechen.stylish.MainMvpController.MEN;
+import static app.waynechen.stylish.MainMvpController.WOMEN;
+import static app.waynechen.stylish.data.source.bean.GetProductList.FIRST_PAGING;
+import static app.waynechen.stylish.data.source.bean.GetProductList.NO_MORE_PAGING;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Created by Wayne Chen on Feb. 2019.
+ */
+public class FavoriteFragment extends Fragment implements CatalogItemContract.View {
+
+    private CatalogItemContract.Presenter mPresenter;
+    private FavoriteItemAdapter mFavoriteItemAdapter;
+
+    private String mItemType;
+    private int mPaging;
+
+    public FavoriteFragment() {}
+
+    public static FavoriteFragment newInstance() {
+        return new FavoriteFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setPaging(FIRST_PAGING);
+        mFavoriteItemAdapter = new FavoriteItemAdapter(mPresenter, mItemType);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void setPresenter(CatalogItemContract.Presenter presenter) {
+        mPresenter = checkNotNull(presenter);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mPresenter.result(requestCode, resultCode);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_catalog_child, container, false);
+
+        RecyclerView recyclerView = root.findViewById(R.id.recycler_catalog_child);
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if (hasNextPaging() && position == mFavoriteItemAdapter.getItemCount() - 1) {
+                    return 2;
+                } else {
+                    return 1;
+                }
+            }
+        });
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2,
+                getContext().getResources().getDimensionPixelSize(R.dimen.space_catalog_grid), true));
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (!recyclerView.canScrollVertically(1)) {
+                    Log.d(Constants.TAG, "Scroll to bottom");
+                    mPresenter.onCatalogItemScrollToBottom(mItemType);
+                }
+            }
+        });
+        recyclerView.setAdapter(mFavoriteItemAdapter);
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (getPaging() == FIRST_PAGING) {
+
+            switch (mItemType) {
+                case WOMEN:
+                    mPresenter.loadWomenProductsData();
+                    break;
+                case MEN:
+                    mPresenter.loadMenProductsData();
+                    break;
+                case ACCESSORIES:
+                    mPresenter.loadAccessoriesProductsData();
+                    break;
+                default:
+            }
+        }
+    }
+
+    @Override
+    public void showProductsUi(GetProductList bean) {
+        setPaging(bean.getPaging());
+        mFavoriteItemAdapter.updateData(bean.getProducts());
+    }
+
+    @Override
+    public boolean hasNextPaging() {
+        return mPaging != NO_MORE_PAGING;
+    }
+
+    @Override
+    public int getPaging() {
+        return mPaging;
+    }
+
+    private void setPaging(int paging) {
+        mPaging = paging;
+    }
+
+    public void setItemType(@MainMvpController.CatalogItem String itemType) {
+        mItemType = itemType;
+    }
+}
